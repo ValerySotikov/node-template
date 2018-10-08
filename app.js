fd256fbc09561d6ee6959a2b75e9bd42af7960ed
imperivm_trade_02 <valery.sotikov@imperivm.team>
2018-10-08 06:28:42 +0500

    last progress

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const app = express();
 
 app.set('PORT', process.env.PORT || 3000);
 
-app.listen(
+require('./launch/db')();
+
+module.exports = app.listen(
   app.get('PORT'),
   (req, res) => {
     console.log(`Server started at port ${app.get('PORT')}...`);
   }
 );
+
---
+++ launch/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+module.exports = () => {
+  const db = config.get('db');
+  mongoose.connect(db).then(() => {
+    console.log(`Connected to ${db}`);
+  }). catch(err => {
+    console.log('Couldn\'t connect to MongoDB');
+  });
+}
